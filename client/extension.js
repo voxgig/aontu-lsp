328bc07d42890e8f01d68a3a67648d27d34850a6
lilsweetcaligula <15699226+lilsweetcaligula@users.noreply.github.com>
2024-06-21 01:31:36 +0300

    Make some changes

--- client/extension.js
+++ client/extension.js
@@ -1,33 +1,34 @@
 // The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+const VSCode = require('vscode')
+
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-
 /**
- * @param {vscode.ExtensionContext} context
+ * @param {VSCode.ExtensionContext} ctx
  */
-function activate(context) {
+function activate(ctx) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "helloworld-minimal-sample" is now active!');
+	console.log('qwerty qwerty');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+	const disposable = VSCode.commands.registerCommand('extension.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+		VSCode.window.showInformationMessage('Hello QWERTY')
 	});
 
-	context.subscriptions.push(disposable);
+	ctx.subscriptions.push(disposable)
 }
+
 
 // this method is called when your extension is deactivated
 function deactivate() {}
+
 
 // eslint-disable-next-line no-undef
 module.exports = {
